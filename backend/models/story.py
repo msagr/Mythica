829a8d2d34552c5495638471e83acd85894fe364
msagr <mehulsagrawal33@gmail.com>
2025-08-10 11:10:20 +0530

    added story model

--- backend/models/story.py
+++ backend/models/story.py
@@ -2,5 +2,26 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, J
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
-class Story():
-    pass
+from db.database import Base
+
+class Story(Base):
+    __tablename__ = "stories"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    session_id = Column(String, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    nodes = relationship("StoryNode", back_populate="story") #One to many relationship
+
+class StoryNode(Base):
+    __tablename__ = "story_nodes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    story_id = Column(Integer, ForeignKey("stories.id"), index=True)
+    content = Column(String)
+    is_root = Column(Boolean, default=False)
+    is_ending = Column(Boolean, default=False)
+    is_winning_ending = Column(Boolean, default=False)
+
+    story = relationship("Story", back_populate="nodes")
