49f10461b542de5afc1fe464d474fac4ec19ca09
msagr <mehulsagrawal33@gmail.com>
2025-08-10 11:10:20 +0530

    db setup

---
+++ backend/models/story.py
@@ -0,0 +1,6 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+class Story():
+    pass
